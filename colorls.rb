28438766a1544617463a225b8e39eb5889825120
Athitya <athityakumar@gmail.com>
2017-06-30 23:21:52 +0530

    Adds support to all ls features and options
    Yet to fix rubocop offenses

--- colorls.rb
+++ colorls.rb
@@ -3,12 +3,24 @@ require 'yaml'
 require 'facets'
 require 'terminfo'
 
+def print(input, color=:white)
+  Kernel.print(input.to_s.colorize(color))
+end
+
+def puts(input, color=:white)
+  Kernel.puts(input.to_s.colorize(color))
+end
+
 # Source for icons unicode: http://nerdfonts.com/
 class ColorLS
-  def initialize(input)
-    @input       = input || Dir.pwd
-    @contents    = Dir.entries(@input) - ['.', '..']
-    @count       = { folders: 0, recognized_files: 0, unrecognized_files: 0 }
+  def initialize(*inputs)
+    flag = inputs.map{ |x| x.start_with?('-') ? 1 : 0}.sum
+    if flag == 1
+      @inputs = inputs[1..-1]
+    else
+      @inputs = inputs[0..-1]
+    end
+    @contents    = `ls #{inputs.join(' ')}`.split("\n").map { |x| x.split(' ') }.to_a
     @formats     = load_from_yaml('formats.yaml').symbolize_keys
     @aliases     = load_from_yaml('aliases.yaml')
                    .to_a
@@ -16,42 +28,69 @@ class ColorLS
                    .to_h
     @format_keys = @formats.keys
     @aliase_keys = @aliases.keys
+    @all_keys    = @format_keys + @aliase_keys
   end
 
   def ls
-    chunkify
-    @contents.each { |chunk| ls_line(chunk) }
-    display_report
+    print "\n"
+    @inputs_count = @inputs.count == 0 ? 1 : @inputs.count
+    i = 0
+    @count       = { 
+      folders: [0]*@inputs_count,
+      recognized_files: [0]*@inputs_count,
+      unrecognized_files: [0]*@inputs_count
+    }
+    @contents.each do |chunk|
+
+      if chunk.empty?
+        puts "\n\t"
+        display_report i
+        i += 1
+      else       
+        chunk.each do |content|
+          input = @inputs[i] || Dir.pwd
+          if Dir.exist?("#{input}/#{content}") || %w[. ..].include?(content)
+            show(content, :folder, :blue, :folders, i)
+          elsif File.exist?("#{input}/#{content}")
+            key = content.split('.').last.downcase.to_sym
+
+            if !@all_keys.include?(key)
+              show(content, :file, :yellow, :unrecognized_files, i)
+            else
+              key = @aliases[key] unless @format_keys.include?(key)
+              show(content, key, :green, :recognized_files, i)
+            end
+          else
+            print "#{content} \t"
+          end
+        end
+        print "\n"
+      end
+    end
+
+    display_report i
+    true
   end
 
   private
 
-  def chunkify
-    @screen_width = TermInfo.screen_size.last
-    @max_width    = @contents.map(&:length).max
-    chunk_size    = @screen_width / @max_width
-    chunk_size   -= 1 while chunk_size * (@max_width + 16) > @screen_width
-    @contents     = @contents.each_slice(chunk_size).to_a
+  def display_report(i)
+    total = @count.values.transpose[i].sum
+    print "\nFound #{total} contents in directory "
+
+    print File.expand_path(@inputs[i] || Dir.pwd).to_s, :blue
+
+    puts  "\n\n\tFolders\t\t\t: #{@count[:folders][i]}"\
+      "\n\tRecognized files\t: #{@count[:recognized_files][i]}"\
+      "\n\tUnrecognized files\t: #{@count[:unrecognized_files][i]}\n"
   end
 
-  def display_report
-    print "\n\nFound #{@contents.flatten.length} contents in directory "
-      .colorize(:white)
-
-    print File.expand_path(@input).to_s.colorize(:blue)
-
-    puts  "\n\n\tFolders\t\t\t: #{@count[:folders]}"\
-      "\n\tRecognized files\t: #{@count[:recognized_files]}"\
-      "\n\tUnrecognized files\t: #{@count[:unrecognized_files]}"
-      .colorize(:white)
-  end
-
-  def fetch_string(content, key, color, increment)
-    @count[increment] += 1
+  def show(content, key, color, increment, i)
+    @count[increment][i] += 1
 
     value = @formats[key]
     logo  = value.gsub(/\\u[\da-f]{4}/i) { |m| [m[-4..-1].to_i(16)].pack('U') }
-    "#{logo}  #{content}".colorize(color)
+    print "#{logo}  #{content} \t", color
   end
 
   def load_from_yaml(filename)
@@ -59,28 +98,7 @@ class ColorLS
     path = prog.include?('/colorls.rb') ? prog.gsub('/colorls.rb', '') : '.'
     YAML.safe_load(File.read("#{path}/#{filename}"))
   end
-
-  def ls_line(chunk)
-    print "\n"
-    chunk.each do |content|
-      print fetch_string(content, *options(content))
-      print ' ' * (@max_width - content.length)
-      print "\t\t"
-    end
-  end
-
-  def options(content)
-    return %i[folder blue folders] if Dir.exist?("#{@input}/#{content}")
-
-    all_keys = @format_keys + @aliase_keys
-    key = content.split('.').last.downcase.to_sym
-
-    return %i[file yellow unrecognized_files] unless all_keys.include?(key)
-
-    key = @aliases[key] unless @format_keys.include?(key)
-    [key, :green, :recognized_files]
-  end
 end
 
-ColorLS.new(ARGV[0]).ls
+ColorLS.new(*ARGV).ls
 true
