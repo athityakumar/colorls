46f86a44bcfcd0b57d2a71c1f7a7d9a4c45bb52a
GladOSkar <okkathe97th@gmail.com>
2017-07-10 11:35:59 +0200

    add -l / --long flag

--- lib/colorls.rb
+++ lib/colorls.rb
@@ -2,6 +2,8 @@ require 'colorize'
 require 'yaml'
 require 'facets'
 require 'terminfo'
+require 'etc'
+require 'filesize'
 
 require 'colorls/core'
 require 'colorls/flags'
--- lib/colorls/core.rb
+++ lib/colorls/core.rb
@@ -1,7 +1,7 @@
 module ColorLS
   class Core
     def initialize(input=nil, all: false, report: false, sort: false, show: false,
-      one_per_line: false)
+      one_per_line: false, long: false)
       @input        = input || Dir.pwd
       @count        = {folders: 0, recognized_files: 0, unrecognized_files: 0}
       @all          = all
@@ -9,6 +9,7 @@ module ColorLS
       @sort         = sort
       @show         = show
       @one_per_line = one_per_line
+      @long         = long
       @screen_width = ::TermInfo.screen_size.last
 
       init_contents
@@ -37,8 +38,28 @@ module ColorLS
       @contents.keep_if { |x| !x.start_with? '.' } unless @all
       filter_contents if @show
       sort_contents   if @sort
-
+      
       @total_content_length = @contents.length
+      
+      return unless @long
+      
+      @userlength = @contents.map{ |c|
+        begin
+          user = Etc.getpwuid(File.stat(c).uid).name
+        rescue ArgumentError
+          user = File.stat(c).uid
+        end
+        user.to_s.length
+      }.max
+
+      @grouplength = @contents.map{ |c|
+        begin
+          group = Etc.getgrgid(File.stat(c).gid).name
+        rescue ArgumentError
+          group = File.stat(c).gid
+        end
+        group.to_s.length
+      }.max
     end
 
     def filter_contents
@@ -83,7 +104,7 @@ module ColorLS
     end
 
     def chunkify
-      return @contents.zip if @one_per_line
+      return @contents.zip if @one_per_line || @long
 
       chunk_size = @contents.count
 
@@ -119,12 +140,51 @@ module ColorLS
         .colorize(:white)
     end
 
+    def long_info(content)
+      stat = File.stat(content)
+      modestr = ""
+      stat.mode.to_s(2).rjust(16, "0")[-9..-1].each_char.with_index do |c, i|
+        if c == '0'
+          modestr += '-'.colorize(:gray)
+        else
+          modestr += "r".colorize(:yellow)  if i % 3 == 0
+          modestr += "w".colorize(:magenta) if i % 3 == 1
+          modestr += "x".colorize(:cyan)    if i % 3 == 2
+        end
+      end
+
+      begin
+        user = Etc.getpwuid(stat.uid).name
+      rescue ArgumentError
+        user = stat.uid
+      end
+      user = user.to_s.ljust(@userlength, " ")
+      user = user.colorize(:green) if user == Etc.getlogin
+
+      begin
+        group = Etc.getgrgid(stat.gid).name
+      rescue ArgumentError
+        group = stat.gid
+      end
+      group = group.to_s.ljust(@grouplength, " ")
+      group = group.colorize(:green) if group == Etc.getlogin
+
+      size = Filesize.from("#{stat.size} B").pretty.split(" ")
+      size = "#{size[0][0..-4].rjust(3," ")} #{size[1].ljust(3," ")}"
+
+      mtime = stat.mtime.asctime
+      mtime = mtime.colorize(:yellow) if (Time.now - stat.mtime < 24 * 60 * 60)
+      mtime = mtime.colorize(:green)  if (Time.now - stat.mtime < 60 * 60)
+
+      "#{modestr} #{user} #{group} #{size} #{mtime}"
+    end
+
     def fetch_string(content, key, color, increment)
       @count[increment] += 1
       value = increment == :folders ? @folders[key] : @files[key]
       logo  = value.gsub(/\\u[\da-f]{4}/i) { |m| [m[-4..-1].to_i(16)].pack('U') }
 
-      "#{logo}  #{content}".colorize(color)
+      "#{logo.colorize(color)}  #{@long ? long_info(content) : ""} #{content.colorize(color)}"
     end
 
     def load_from_yaml(filename, aliase=false)
