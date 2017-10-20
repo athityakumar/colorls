1f36404ae8445a6268129c488758ae26dc132fd6
Claudio Bley <cbley@exa-online.de>
2017-10-20 22:19:56 +0200

    Fix handling of `--tree` and `--all` / `--allmost_all` flags
    When using `:tree` mode, the `:allmost_all` was always enabled unconditionally.

--- lib/colorls/flags.rb
+++ lib/colorls/flags.rb
@@ -28,11 +28,11 @@ module ColorLS
       return unless @mode == :tree
 
       # FIXME: `--all` and `--tree` do not work together, use `--almost-all` instead
+      @opts[:almost_all] = true if @opts[:all]
       @opts[:all] = false
-      @opts[:almost_all] = true
 
       # `--tree` does not support reports
-      @opts[:report] = false if @mode == :tree
+      @opts[:report] = false
     end
 
     def process
