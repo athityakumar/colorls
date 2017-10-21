3b783382f594833921cd18f353fb63503d0abbf8
Henré Botha <henrebotha@gmail.com>
2017-10-21 23:49:30 +0200

    Consolidate test files

--- spec/colorls_spec.rb
+++ spec/colorls_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe ColorLS do
   it 'has a version number' do
     expect(ColorLS::VERSION).not_to be nil
   end
+
+  it 'lists info of a hidden file with --long option' do
+    expect { ColorLS::Flags.new('--long', 'spec/fixtures/.hidden-file').process }.to_not output(/No Info/).to_stdout
+  end
 end
