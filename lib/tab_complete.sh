e002eb0cb5d0853063794e19b22a5ff5913fec98
Claudio Bley <claudio.bley@gmail.com>
2017-10-27 13:30:55 +0200

    Use optparse for tab completion

--- lib/tab_complete.sh
+++ lib/tab_complete.sh
@@ -1,4 +1,4 @@
-_colorls_options='-1 -a -A -d -f -l -r -t -h
---all --almost-all --dirs --files --long --report --sort-dirs --group-directories-first
---sort-files --git-status --tree --help --sd --sf --gs'
-complete -W "${_colorls_options}" 'colorls'
+function _colorls_complete() {
+    COMPREPLY=( $( colorls --'*'-completion-bash="$2" ) )
+}
+complete -o default -F _colorls_complete colorls
