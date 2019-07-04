c563967dd957ca6905485de53bdcc9525255b595
Andrew <web-meister@mail.ru>
2019-07-04 20:29:04 +0300

    Update core.rb

--- lib/colorls/core.rb
+++ lib/colorls/core.rb
@@ -190,11 +190,7 @@ module ColorLS
       format_mode(m >> 6, stat.setuid?, 's') +
         format_mode(m >> 3, stat.setgid?, 's') +
         format_mode(m, stat.sticky?, 't')
-    end
-    
-    def nlink(nlink)
-      nlink.to_s.rjust(4, ' ')
-    end     
+    end    
 
     def user_info(content)
       content.owner.ljust(@userlength, ' ').colorize(@colors[:user])
@@ -261,7 +257,7 @@ module ColorLS
     def long_info(content)
       return '' unless @long
 
-      [mode_info(content.stats), nlink(content.nlink), user_info(content), group_info(content.group),
+      [mode_info(content.stats), content.nlink.to_s.rjust(4, ' '), user_info(content), group_info(content.group),
        size_info(content.size), mtime_info(content.mtime)].join('  ')
     end
 
