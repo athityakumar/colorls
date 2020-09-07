cfee52a261b8ce3126496c1932ae5a4c1298c1c0
jaredyam <jihonren@gmail.com>
2020-09-07 19:39:54 +0800

    Fix the code style

--- lib/colorls/core.rb
+++ lib/colorls/core.rb
@@ -313,7 +313,7 @@ module ColorLS
                   when file.blockdev? then :blockdev
                   when file.socket? then :socket
                   else
-                    (key != :file and @files.key?(key)) ? :recognized_file : :unrecognized_file
+                    key != :file && @files.key?(key) ? :recognized_file : :unrecognized_file
                   end
       @colors[color_key]
     end
@@ -330,7 +330,7 @@ module ColorLS
         key = @file_aliases[key] unless @files.key? key
         key = :file if key.nil?
         color = file_color(content, key)
-        group = (key != :file and @files.key?(key)) ? :recognized_files : :unrecognized_files
+        group = key != :file && @files.key?(key) ? :recognized_files : :unrecognized_files
       end
 
       [key, color, group]
