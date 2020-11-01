6c697cdb9e986ed65ec02b33d0a3cfc64e32d039
Claudio Bley <claudio.bley@gmail.com>
2020-11-01 21:28:36 +0100

    Remove `symbolize_keys` monkey method on `Hash`
    Usage in a single place does not really warrant a monkey patch and it
    can be replaced with `transform_keys!` easily.

--- lib/colorls/monkeys.rb
+++ lib/colorls/monkeys.rb
@@ -9,13 +9,3 @@ class String
     split('').uniq.join
   end
 end
-
-class Hash
-  def symbolize_keys
-    new_hash = {}
-    each_key do |key|
-      new_hash[key.to_sym] = delete(key)
-    end
-    new_hash
-  end
-end
