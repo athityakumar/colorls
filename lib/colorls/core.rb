4c1824c3cfd4ad386e3c1e6bf8cf8b02193f7043
Claudio Bley <claudio.bley@gmail.com>
2024-09-05 22:47:17 +0200

    Simplify implementation

--- lib/colorls/core.rb
+++ lib/colorls/core.rb
@@ -260,7 +260,7 @@ module ColorLS
 
     def size_info(filesize)
       filesize = Filesize.new(filesize)
-      size = @show_human_readable_size ? filesize.pretty : filesize.to_s
+      size = @show_human_readable_size ? filesize.pretty(precision: 0) : filesize.to_s('B', precision: 0)
       size = size.split
       size = justify_size_info(size)
       return size.colorize(@colors[:file_large])  if filesize >= 512 * (1024 ** 2)
@@ -280,19 +280,9 @@ module ColorLS
     end
 
     def justify_size_info(size)
-      size_num = displayable_size_num(size[0]).rjust(chars_for_size, ' ')
+      size_num = size[0].rjust(chars_for_size, ' ')
       size_unit = @show_human_readable_size ? size[1].ljust(3, ' ') : size[1]
       "#{size_num} #{size_unit}"
-    end
-
-    def displayable_size_num(size_num_str)
-      digit_after_decimal = size_num_str[-2]
-      if %w[5 6 7 8 9].include?(digit_after_decimal)
-        num = size_num_str.to_i
-        return (num + 1).to_s
-      end
-
-      size_num_str[0..-4]
     end
 
     def clear_chars_for_size
